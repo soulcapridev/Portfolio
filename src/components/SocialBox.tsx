fda99c06cb3bf509c6e520df707ab88e7405477e
SoulCapri | Rust Engineer <misssilverbeauty0927@outlook.com>
2025-10-03 15:04:00 -0400

    Update SocialBox.tsx

--- src/components/SocialBox.tsx
+++ src/components/SocialBox.tsx
@@ -10,12 +10,12 @@ import {
 const SocialBox: FC = () => {
   return (
     <div className="flex flex-wrap gap-4 mt-4 justify-center">
-      <Link href="https://x.com/rustpump951227" passHref target="_blank">
+      <Link href="https://x.com/soulcapridev" passHref target="_blank">
         <div className="grid w-10 h-10 p-2 rounded-3xl place-content-center shadow-item dark:shadow-none dark:bg-white">
           <TwitterIcon />
         </div>
       </Link>
-      <Link href="https://github.com/jwest951227" passHref target="_blank">
+      <Link href="https://github.com/soulcapridev" passHref target="_blank">
         <div className="grid w-10 h-10 p-2 rounded-3xl place-content-center shadow-item dark:shadow-none dark:bg-white">
           <GithubIcon />
         </div>
@@ -29,7 +29,7 @@ const SocialBox: FC = () => {
           <DiscordHIcon />
         </div>
       </Link> */}
-      <Link href="https://t.me/jwest951227" passHref target="_blank">
+      <Link href="https://t.me/soulcapridev" passHref target="_blank">
         <div className="w-10 h-10 flex items-center justify-center rounded-3xl mr-2.5 card-shadow mb-2.5 shadow-item dark:bg-white dark:shadow-none">
           <TelegramHIcon />
         </div>
