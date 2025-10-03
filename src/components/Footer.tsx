8f7df7952d6c071c66aa37896b6c64af25b26274
SoulCapri | Rust Engineer <misssilverbeauty0927@outlook.com>
2025-10-03 15:07:11 -0400

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,7 @@ const Footer: FC = () => {
     <footer id="contact">
       <div className="mt-20">
         <p className="mb-5 font-serif px-6 !text-gray-800 dark:!text-gray-500">
-          © 2024 Jay West
+          © 2025 Soul Capricorn
         </p>
       </div>
     </footer>
