aac034ff2f6e96506802e56b74611f754560fe9d
SoulCr@ncr | Rust Engineer <misssilverbeauty0927@outlook.com>
2025-10-15 13:58:19 -0400

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,7 @@ const Footer: FC = () => {
     <footer id="contact">
       <div className="mt-20">
         <p className="mb-5 font-serif px-6 !text-gray-800 dark:!text-gray-500">
-          © 2025 Soul Capricorn
+          © 2025 Soul Cr@ncr
         </p>
       </div>
     </footer>
